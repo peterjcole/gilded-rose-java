63123ec4e6d26efd4f48df306b35a9118bd04491
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-10-16 10:12:44 +0100

    add missing test cases and extract some methods

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -42,6 +42,15 @@ public class GildedRoseTest {
         assertEquals(-2, app.items[0].sellIn);
     }
 
+        @Test
+    public void updateQuality_shouldDegradeByTwo_whenSellInIsZero() {
+        Item[] items = new Item[] { new Item("standard item", 0, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(3, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
     @Test
     public void updateQuality_shouldIncreaseQualityOfAgedBrie() {
         Item[] items = new Item[] { new Item("Aged Brie", 5, 5) };
@@ -59,6 +68,15 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals(7, app.items[0].quality);
         assertEquals(-2, app.items[0].sellIn);
+    }
+
+        @Test
+    public void updateQuality_shouldIncreaseQualityOfAgedBrieByTwo_whenSellInIsZero() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(7, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
     }
 
     @Test
@@ -82,6 +100,14 @@ public class GildedRoseTest {
         assertEquals(10, app.items[0].sellIn);
         assertEquals(80, app.items[1].quality);
         assertEquals(-1, app.items[1].sellIn);
+    }
+
+    @Test
+    public void updateQuality_shouldIncreaseQualityForBackstagePasses() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 15, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
     }
 
     @Test
@@ -119,6 +145,24 @@ public class GildedRoseTest {
     @Ignore
     @Test
     public void updateQuality_shouldWorkForMultipleItems() {
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+                // this conjured item does not work properly yet
+                // new Item("Conjured Mana Cake", 3, 6) };
+        };
+        GildedRose app = new GildedRose(items);
 
+        app.updateQuality();
+        assertEquals(19, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(4, app.items[1].quality);
+        assertEquals(-1, app.items[1].sellIn);
+        assertEquals(80, app.items[2].quality);
+        assertEquals(-1, app.items[2].sellIn);
+        assertEquals(22, app.items[3].quality);
+        assertEquals(15, app.items[3].sellIn);
     }
 }
