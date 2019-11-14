4f3e5ac9d86ade86aabcab509e57b1d9de1baaa5
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-11-14 14:43:37 +0000

    strategies are now being used

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,62 +11,11 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            handleItem(item);
-        }
-    }
-
-    public void handleItem(Item item) {
-        try {
-            switch (item.name) {
-                case "Aged Brie":
-                    handlePreservedItem(item);
-                    break;
-                case "Sulfuras, Hand of Ragnaros":
-                    handleEpicItem(item);
-                    break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    handleDeadlineItem(item);
-                    break;
-                default:
-                    handleStandardItem(item);
+            try {
+                StockChecker.examine(item).update(item);
+            } catch (DeadlineItemExpiredException e) {
+                System.out.println("You have expired items, chuck them!");
             }
-        } catch (DeadlineItemExpiredException e) {
-
-        }
-    }
-
-    public void handleStandardItem(Item item) {
-        reduceSellInOf(item);
-        int qualityDecrease = (isExpired(item) ? -2 : -1);
-        adjustQuality(item, qualityDecrease);
-    }
-
-    public void handlePreservedItem(Item item) {
-        reduceSellInOf(item);
-        int qualityIncrease = (isExpired(item)) ? 2 : 1;
-        adjustQuality(item, qualityIncrease);
-    }
-
-    public void handleEpicItem(Item item) {}
-
-    public void handleDeadlineItem(Item item) {
-        reduceSellInOf(item);
-
-        if(isExpired(item)) {
-            throw new DeadlineItemExpiredException(item); 
-        }
-
-        int qualityIncrease = calculateDeadlineQualityIncrease(item);
-        adjustQuality(item, qualityIncrease);
-    }
-
-    private int calculateDeadlineQualityIncrease(Item item) {
-        if (isCoveted(item)) {
-            return 3;
-        } else if (isInDemand(item)) {
-            return 2;
-        } else {
-            return 1;
         }
     }
 }
